68b441ba4df6fd5bcd44b188940b37e2ee06f9ee
Юлия Левина <ombro.de.morto@gmail.com>
2021-05-10 23:09:09 +0300

    First Commit

---
+++ HighLevel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HighLevel
+{
+    public class Program
+    {
+        static void Main()
+        {
+            double firstResult;
+            double firstArg = 1;
+            string readArg = "";
+
+            readArg = Console.ReadLine();
+            firstArg = Convert.ToDouble(readArg);
+            firstResult = GetFirstFunction(firstArg);
+
+            Console.WriteLine("FirstResult = " + firstResult);
+            Console.ReadKey();
+        }
+
+        public static double GetFirstFunction(double arg)
+        {
+            double result = Math.Cos(arg) + Math.Sin(arg) + Math.Cos(3 * arg) + Math.Sin(3 * arg);
+
+            return result;
+        }
+    }
+}
